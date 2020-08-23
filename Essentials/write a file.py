3fe38868ea2f1fb8f5d075fe42b04c9f53255bb6
karanpuri0010 <66718194+karanpuri0010@users.noreply.github.com>
2020-08-23 20:18:16 +0530

    Update write a file.py

--- Essentials/write a file.py
+++ Essentials/write a file.py
@@ -2,17 +2,16 @@
 # Otherwise write() func will not work.
 # if file doesnt exists it will create a new one
 
-file = open("/Users/karpuri/Documents/Devnet/ATA/test1.txt","w")
+file = open("/Users/karpuri/Documents/Devnet/test1.txt","w")
 file.write("I am writing the file")
 file.close()
 
 # To append text use "a"
-file1 = open("/Users/karpuri/Documents/Devnet/ATA/test1.txt", "a")
+file1 = open("/Users/karpuri/Documents/Devnet/test1.txt", "a")
 file1.write("New line entry")
 file1.close()
 
 # view the file
-file1 = open("/Users/karpuri/Documents/Devnet/ATA/test1.txt")
+file1 = open("/Users/karpuri/Documents/Devnet/test1.txt")
 print(file1.read())
 file1.close()
-
