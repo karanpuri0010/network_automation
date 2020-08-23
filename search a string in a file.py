648d742fa0dd056c4634f7b3ec6b7c8eb3f1c041
karanpuri0010 <66718194+karanpuri0010@users.noreply.github.com>
2020-08-23 20:06:43 +0530

    Update search a string in a file.py

--- search a string in a file.py
+++ search a string in a file.py
@@ -2,7 +2,7 @@
 
 import re
 
-file = open("/Users/karpuri/Documents/Devnet/ATA/20190812-064347-admin-tech/var/log/messages.2")
+file = open("/Users/karpuri/Documents/Devnet/20190812-064347-admin-tech/var/log/messages.2")
 
 for x in file:
     if re.search("10ge0/0", x):
