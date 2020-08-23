82929900cac2303d96e658e04de072aa1db0ab62
karanpuri0010 <66718194+karanpuri0010@users.noreply.github.com>
2020-08-23 20:17:02 +0530

    Update search a string in a file.py

--- Essentials/search a string in a file.py
+++ Essentials/search a string in a file.py
@@ -2,11 +2,11 @@
 
 import re
 
-file = open("/Users/karpuri/Documents/Devnet/ATA/20190812-064347-admin-tech/var/log/messages.2")
+file = open("/Users/karpuri/Documents/Devnet/admin-tech/var/log/messages.2")
 
 for x in file:
     if re.search("10ge0/0", x):
-        save = open("/Users/karpuri/Documents/Devnet/ATA/save.txt", "a")
+        save = open("/Users/karpuri/Documents/Devnet/save.txt", "a")
         save.write(x)
         save.close()
         
